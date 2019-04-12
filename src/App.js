bb3e664260d880b61f2134eab89fb43c22c33350
by_6 <bysiek1998@gmail.com>
2019-04-12 18:56:49 +0200

    clearing default react App.js and App.css adding ToDoList class and first 3 tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class ToDoList extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React :)
-          </a>
-        </header>
+      <div>
+        <h1>{this.props.title}</h1>
+        <div>
+          <ul>
+            {this.props.tasks.map(task =><li>{ task }</li> )}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  myTasks = [
+    'Write neural network',
+    'Make map in illustrator',
+    'Go date with Ola'
+  ]
+
+  render() {
+    return (
+      <div>
+        < ToDoList title = 'My list'
+        tasks = { this.myTasks }
+        />
       </div>
     );
   }
